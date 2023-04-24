20c0289712b9cb3d50c91f4bffbf8e8950d4a647
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-04-24 11:00:21 +0900

    ✨ :: 404페이지 설정

--- src/components/router/index.jsx
+++ src/components/router/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route,  Routes as Switch} from "react-router-dom";
 import MainPage from "../Main";
+import NotFoundPage from '../../pages/NotFoundPage';
 
 const Router = () => {
   return (
     <>
         <Switch>
             <Route path="/" element={<MainPage/>}/>
+            <Route path='/*' element={<NotFoundPage/>}/>
         </Switch>
     </>
   )
