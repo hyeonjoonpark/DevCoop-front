a7fe4bb1f0d0869269c4f3ce785b68e77d5e2dc9
HyeJeong0620 <catlqweasd0620@gmail.com>
2023-05-26 00:10:44 +0900

    FEAT : 제목 및 버튼 생성

--- src/components/Studentinfo/index.jsx
+++ src/components/Studentinfo/index.jsx
@@ -11,10 +11,25 @@ const Studentinfo = () => {
             <Header />
             <_.InfoContainer>
                 <_.InfoHeader>
-                    <_.Infotitle>학생충전페이지</_.Infotitle>
+                    <_.Infotitle style={{marginLeft: "10px"}}>학생 정보 조회</_.Infotitle>
+                    <_.Infobutton style={{ marginLeft: "250px", marginRight: "10px" }}>일괄선택</_.Infobutton>
+                    <_.Infobutton>일괄충전</_.Infobutton>
+                    <_.Infoline style={{marginTop: "20px"}}/>
                 </_.InfoHeader>
                 <_.Infolist>
-
+                    <_.Infotext color='8A8A8A'>선택</_.Infotext>
+                    <_.Infotext>학번</_.Infotext>
+                    <_.Infotext>이름</_.Infotext>
+                    <_.Infotext>바코드번호</_.Infotext>
+                    <_.Infotext>관리자</_.Infotext>
+                    <_.Infoline style={{marginTop: "20px"}}/>
+                    <_.Infodata>
+                        <_.Inforadiobutton><input type='radio'/></_.Inforadiobutton>
+                        <_.Infodatatext>2206</_.Infodatatext>
+                        <_.Infodatatext>김알이</_.Infodatatext>
+                        <_.Infodatatext>000</_.Infodatatext>
+                        <_.Infodatatext>관리자</_.Infodatatext>
+                    </_.Infodata>
                 </_.Infolist>
             </_.InfoContainer>
         </P.PageContainer>
--- src/components/Studentinfo/style.js
+++ src/components/Studentinfo/style.js
@@ -17,13 +17,51 @@ export const Infolist = styled.div`
     height: 100%;
 `;
 
-export const Infotitle = styled.h1`
+export const Infotitle = styled.text`
+    margin: 0 auto;
     font-size: 40px;
+    font-weight: bold;
     color: #333;
 `;
 
 export const Infobutton = styled.button`
+    margin: 0 auto;
+    font-size: 20px;
     width: 180px;
     height: 55px;
     color: 41434C;
+    border-radius: 5px;
+`;
+
+export const Infoline = styled.hr`
+    margin: 0 auto;
+    width: 865px;
+`;
+
+export const Infotext = styled.text`
+    margin: 0 45px auto 45px;
+    font-size: 30px;
+    font-weight: bold;
+    color: ${props => props.color || '#333'};
+`;
+
+export const Infodata = styled.div`
+    margin: 5px auto;
+    width: 865px;
+    height: 100px;
+    background-color: pink;
+    border-radius: 5px;
+`;
+
+export const Inforadiobutton = styled.button`
+    margin: 0 45px 0 45px;
+`;
+
+export const Infodatatext = styled.text`
+    margin: 0 45px auto 45px;
+    font-size: 30px;
+    font-weight: bold;
+    line-height: 100px;
+    text-align: center;
+    color: ${props => props.color || '#333'};
 `;
