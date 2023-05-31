97a100e5f028de0a9f4372d9cc249dd9f3c2a515
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-06-01 00:03:18 +0900

    FIX : Logo 수정 및 로그인 여부에 따른 조건부 렌더링

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,24 @@
 import * as H from "../../common/PageWrapStyle";
-import { ReactComponent as AriPayLogo } from "../../assets/AriPayL.svg";
+import { ReactComponent as AriPayLogo } from "../../assets/AriPayL_ver2.svg";
 import { ReactComponent as LockLogo } from "../../assets/LockLogo.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <H.PageHeader>
       <H.HeaderInBox>
-        <AriPayLogo width={"70px"} height={"70px"} />
+        <AriPayLogo width={"130px"} height={"130px"} />
       </H.HeaderInBox>
-      <H.HeaderInBox testAlign={"center"} paddingTop={"30px"}>
+      <H.HeaderInBox testAlign={"center"} paddingTop={"50px"}>
         <LockLogo />
-        <Link to="login">로그인</Link>
+        {isLoggedIn ? (
+          <Link to="/">로그아웃</Link>
+        ) : (
+          <Link to="login">로그인</Link>
+        )}
       </H.HeaderInBox>
     </H.PageHeader>
   );
