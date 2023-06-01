4f1607c02cf168ec503fbd8e2ec24d256f3c5921
HyeJeong0620 <catlqweasd0620@gmail.com>
2023-06-01 20:44:45 +0900

    STYLE : style 수정

--- src/components/Studentinfo/style.js
+++ src/components/Studentinfo/style.js
@@ -7,6 +7,8 @@ export const InfoContainer = styled.div`
 `;
 
 export const InfoHeader = styled.div`
+    margin: 0 auto;
+    text-align: center;
     width: 900px;
     height: 100px;
 `;
@@ -22,6 +24,7 @@ export const Infotitle = styled.text`
     font-size: 40px;
     font-weight: bold;
     color: #333;
+    margin: 0 10px;
 `;
 
 export const Infobutton = styled.button`
@@ -39,29 +42,99 @@ export const Infoline = styled.hr`
 `;
 
 export const Infotext = styled.text`
-    margin: 0 45px auto 45px;
     font-size: 30px;
     font-weight: bold;
-    color: ${props => props.color || '#333'};
+    text-align: center;
 `;
 
 export const Infodata = styled.div`
+    display: flex;
+    justify-content: center;
     margin: 5px auto;
     width: 865px;
     height: 100px;
-    background-color: pink;
-    border-radius: 5px;
+    border-radius: 10px;
+    background: ${props => props.background || '#E8EBF5'};
 `;
 
 export const Inforadiobutton = styled.button`
-    margin: 0 45px 0 45px;
+    background: none;
 `;
 
 export const Infodatatext = styled.text`
-    margin: 0 45px auto 45px;
     font-size: 30px;
     font-weight: bold;
     line-height: 100px;
     text-align: center;
     color: ${props => props.color || '#333'};
+`;
+
+export const Info = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 0 0 0 15px;
+    width: 865px;
+    height: 65px;
+`;
+
+export const Infoname = styled.div`
+    width: 150px;
+    height: 65px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infochoose = styled.div`
+    width: 150px;
+    height: 65px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infoclass = styled.div`
+    width: 150px;
+    height: 65px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infonum = styled.div`
+    width: 242px;
+    height: 65px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infodir = styled.div`
+    width: 173px;
+    height: 65px;
+    text-align: center;
+    line-height: 65px;
+`;
+
+export const Infonamedata = styled.div`
+    width: 150px;
+    height: 100px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infochoosedata = styled.div`
+    width: 150px;
+    height: 100px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infoclassdata = styled.div`
+    width: 150px;
+    height: 100px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infonumdata = styled.div`
+    width: 242px;
+    height: 100px;
+    text-align: center;
+    line-height: 65px;
+`;
+export const Infodirdata = styled.div`
+    width: 173px;
+    height: 100px;
+    text-align: center;
+    line-height: 65px;
 `;
