77e3e4609178b032ad3e1e5792fac87735b418eb
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-06-08 08:28:15 +0900

    FEAT : 404페이지 기본 구성(#19)

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as NotFoundLogo } from "../assets/404AriPayC.svg";
+import styled from "styled-components";
 
 export default function NotFoundPage() {
   return (
-    <div>
-            <h1>페이지를 찾을 수 없습니다 :{"("} </h1>
-            <Link to='/'>메인으로 돌아가기</Link>
-    </div>
-  )
+    <NotFoundWrap>
+        <NotFoundLogo width={"400px"} height={"200px"} />
+        <h1>페이지를 찾을 수 없습니다 :{"("} </h1>
+        <Link to="/">메인으로 돌아가기</Link>
+    </NotFoundWrap>
+  );
 }
+
+const NotFoundWrap = styled.p`
+  text-align: center;
+`;
