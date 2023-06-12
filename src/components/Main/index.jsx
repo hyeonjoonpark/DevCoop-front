05604303b2e556e542b8e2958a76f3ca49823db7
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-06-12 14:59:35 +0900

    FIX : 문의하기 디자인 수정

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import * as _ from "./style"
-import * as P from "../../common/PageWrapStyle"
-import Header from '../Header'
+import React from "react";
+import * as _ from "./style";
+import * as P from "../../common/PageWrapStyle";
+import Header from "../Header";
 
 const Main = () => {
   return (
     <>
-    <P.PageWrap>
-      <P.PageContainer>
-        <Header/>
-        <_.maintop>
-          <_.topdiv></_.topdiv>
-          <_.bottomdiv>
-            <_.Infotext>사용 내역이 궁금하다면?</_.Infotext>
-          </_.bottomdiv>
-        </_.maintop>
-        <_.mainbottom>
-          <_.usediv></_.usediv>
-          <_.askdiv></_.askdiv>
-        </_.mainbottom>
-      </P.PageContainer>
-    </P.PageWrap>
-    </>
-  )
-}
+      <P.PageWrap>
+        <P.PageContainer>
+          <Header />
+          <_.maintop>
+            <_.topdiv></_.topdiv>
+            <_.bottomdiv>
+              <_.Infotext>사용 내역이 궁금하다면?</_.Infotext>
+            </_.bottomdiv>
+          </_.maintop>
 
-export default Main
+          <_.Mainbottom>
+            <_.UseBox></_.UseBox>
+            <_.AskBox>
+              <_.AskInTop>
+                <a
+                  href="https://www.instagram.com/bsm_devcoop/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  문의하기
+                </a>
+              </_.AskInTop>
+              <_.CallLogoStyle />
+            </_.AskBox>
+          </_.Mainbottom>
+        </P.PageContainer>
+      </P.PageWrap>
+    </>
+  );
+};
+
+export default Main;
--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -1,54 +1,82 @@
 import styled from "styled-components";
+import { ReactComponent as CallLogo } from "../../assets/CallLogo.svg";
 
 export const maintop = styled.div`
-    margin: 0 auto;
-    width: 1000px;
-    height: 400px;
+  margin: 0 auto;
+  width: 1000px;
+  height: 400px;
 `;
 
-export const mainbottom = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 0 auto;
-    width: 1000px;
-    height: 300px;
-    background: #E3E5E7;
+export const Mainbottom = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 1000px;
+  height: 300px;
+  background: #e3e5e7;
 `;
 
 export const topdiv = styled.div`
-    margin: 0 auto;
-    width: 970px;
-    height: 280px;
-    background: #41434C;
-    border-radius: 50px 50px 0 0;
+  margin: 0 auto;
+  width: 970px;
+  height: 280px;
+  background: #41434c;
+  border-radius: 50px 50px 0 0;
 `;
 
 export const bottomdiv = styled.div`
-    margin: 0 auto;
-    width: 970px;
-    height: 100px;
-    background: #34343C;
-    border-radius: 0 0 50px 50px;
+  margin: 0 auto;
+  width: 970px;
+  height: 100px;
+  background: #34343c;
+  border-radius: 0 0 50px 50px;
 `;
 
-export const usediv = styled.div`
-    margin: 15px 10px 15px 15px;
-    width: 400px;
-    height: 270px;
-    background: #F0CE00;
-    border-radius: 20px;
+export const UseBox = styled.div`
+  margin: 15px 10px 15px 15px;
+  width: 400px;
+  height: 270px;
+  background: #f0ce00;
+  border-radius: 12px;
 `;
 
-export const askdiv = styled.div`
-    margin: 15px 10px 15px 10px;
-    width: 550px;
-    height: 270px;
-    background: white;
-    border-radius: 20px;
+export const AskBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 15px 10px 15px 10px;
+  width: 550px;
+  height: 270px;
+  background: #fff;
+  border-radius: 12px;
 `;
 
 export const Infotext = styled.text`
-    font-size: 25px;
-    text-align: center;
-    color: white;   
+  font-size: 25px;
+  text-align: center;
+  color: white;
+`;
+
+export const AskInTop = styled.div`
+  display: flex;
+  width: 100%;
+  height: 150px;
+  background-color: #fff;
+  border-radius: 12px;
+
+  a {
+    display: block;
+    width: 150px;
+    height: 50px;
+
+    margin: 20px;
+    font-size: 30px;
+    font-weight: 600;
+    color: #8a8a8a;
+  }
+`;
+
+export const CallLogoStyle = styled(CallLogo)`
+  width: 550px;
+  height: 300px;
+  border-radius: 12px;
 `;
