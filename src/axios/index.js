cd1d0c237b2163d99d0a03f355a1c93df549fc07
김규봉 <tfm0405@gmail.com>
2023-06-18 15:23:14 +0900

    인터셉터 기능 추가중

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,63 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://10.129.57.252:6002/api",
+  headrs: {
+    "Content-Type": "application/json",
+    withCredentials: true,
+  }
 });
+
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const accToken = localStorage.getItem("token");
+    const refToken = localStorage.getItem("refreshtoken");
+    if (!accToken) {
+      console.log("no accToken")
+      config.headers["access"] = null;
+      config.headers["refresh"] = null;
+      return config
+    }
+    config.headers["access"] = accToken;
+    config.headers["refresh"] = refToken;
+    return config
+  },
+  function (error) {
+    console.log("interceptor >", error);
+    return Promise.reject(error);
+  }
+)
+
+
+// axiosInstance.interceptors.response.use(
+//   function (response) {
+//     console.log("No problem")
+//       return response
+//   },
+//   async function (error) {
+//     console.log("error")
+//     console.log(error)
+//     if (error.response && error.response.status === 403) {
+//         try {
+//             // const originalRequest = error.config;
+//             // const data = await client.get('auth/refreshtoken')
+//             // if (data) {
+//             //     const {accessToken, refreshToken} = data.data
+//             //     localStorage.setItem('user', JSON.stringify(data.data, ['accessToken', 'refreshToken']))
+//             //     originalRequest.headers['accessToken'] = accessToken;
+//             //     originalRequest.headers['refreshToken'] = refreshToken;
+//             //     return await client.request(originalRequest);
+//             //     }
+//         } catch (error){
+//             localStorage.removeItem('user');
+//             console.log(error);
+//         }
+//         return Promise.reject(error)
+//     }
+//     return Promise.reject(error)
+//   }
+// )
+
 
 export const login = async (email, password) => {
   try {
@@ -24,8 +80,33 @@ export const login = async (email, password) => {
   }
 };
 
+
+
 // 토큰이 없거나 있는데 둘 다 만료된 ->로그인페이지
 // 나머지는 메인
+
+// export const checkToken = (hello) => {
+//   try {
+//     const accToken = localStorage.getItem("token");
+//     const refToken = localStorage.getItem("refreshtoken");
+//     const response = axiosInstance.post(
+//       "/me",
+//       { hello },
+//       {
+//         headers: {
+//           accessToken: `${accToken}`,
+//           refrashToken: `${refToken}`,
+//           "Content-Type": "application/json",
+//         },
+//       }
+//     );
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+
 
 export const checkToken = (hello) => {
   try {
@@ -48,6 +129,8 @@ export const checkToken = (hello) => {
   }
 };
 
+
+
 export const getPoint = async () => {
   try {
     const access_token = localStorage.getItem("token");
@@ -68,3 +151,6 @@ export const getPoint = async () => {
     throw error;
   }
 };
+
+
+export default axiosInstance;
